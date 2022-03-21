0c05816cbdc4ef9ae1de1c473ecaddbf44096096
Elias Santana <eliassilva0045@gmail.com>
2022-03-20 22:16:19 -0300

    Create a greeting app from scratch

---
+++ app/src/main/java/com/example/firststeps/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.firststeps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var title = findViewById<TextView>(R.id.tvTitle)
+        var btn = findViewById<Button>(R.id.button)
+        val etNome = findViewById<EditText>(R.id.etName)
+        btn.setOnClickListener {
+            if(etNome.text.isNotBlank()){
+                title.text = getString(R.string.say_hello, etNome.text.toString())
+            }else{
+                etNome.error = getString(R.string.error_message)
+            }
+        }
+
+    }
+}
