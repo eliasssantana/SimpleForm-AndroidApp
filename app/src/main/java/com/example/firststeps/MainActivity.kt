3d514dd18b04c9afa98d010ce5c50d3cbbb0f122
Elias Santana <eliassilva0045@gmail.com>
2022-03-26 19:19:49 -0300

    Implement SendResultActivity logic

--- app/src/main/java/com/example/firststeps/MainActivity.kt
+++ app/src/main/java/com/example/firststeps/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.firststeps
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,26 +16,50 @@ class MainActivity : AppCompatActivity() {
         val title = findViewById<TextView>(R.id.tvTitle)
         val btn = findViewById<Button>(R.id.button)
         val btn2 = findViewById<Button>(R.id.button2)
+        val btn3 = findViewById<Button>(R.id.button3)
         val etNome = findViewById<EditText>(R.id.etName)
         btn.setOnClickListener {
-            if(etNome.text.isNotBlank()){
+            if (etNome.text.isNotBlank()) {
                 title.text = getString(R.string.say_hello, etNome.text.toString())
                 etNome.text.clear()
-            }else{
+            } else {
                 etNome.error = getString(R.string.error_message)
             }
         }
 
         btn2.setOnClickListener {
-            if(etNome.text.isNotBlank()){
+            if (etNome.text.isNotBlank()) {
                 val nomeDigitado = etNome.text.toString()
                 val intent = Intent(this, ResultActivity::class.java)
                 intent.putExtra("NOME_DIGITADO", nomeDigitado)
                 startActivity(intent)
-            }else{
+            } else {
                 etNome.error = getString(R.string.error_message)
             }
         }
 
+        btn3.setOnClickListener {
+            val intent = Intent(this, SendResultActivity::class.java)
+            val requestCode = 1
+
+            //deprecado. Procura o nova forma de se fazer isso
+            startActivityForResult(intent, requestCode)
+        }
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1){
+            if (resultCode == Activity.RESULT_OK) {
+                val result = data?.getStringExtra("RESULT")
+
+                val title = findViewById<TextView>(R.id.tvTitle)
+                title.text = getString(R.string.data_received, result)
+            }else if(resultCode == Activity.RESULT_CANCELED){
+                val title = findViewById<TextView>(R.id.tvTitle)
+                title.text = getString(R.string.screen_canceled)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/firststeps/SendResultActivity.kt
+++ app/src/main/java/com/example/firststeps/SendResultActivity.kt
@@ -1,11 +1,43 @@
 package com.example.firststeps
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.annotation.StringRes
 
 class SendResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_result)
+
+        val btYes = findViewById<Button>(R.id.btYes)
+        val btNo = findViewById<Button>(R.id.btNo)
+        val btCancel = findViewById<Button>(R.id.btCancel)
+
+        btNo.setOnClickListener{
+            sendResult(R.string.no)
+        }
+
+        btYes.setOnClickListener{
+            sendResult(R.string.yes)
+        }
+
+        btCancel.setOnClickListener{
+            setResult(Activity.RESULT_CANCELED)
+
+            finish()
+        }
+    }
+
+    private fun sendResult(@StringRes stringResId: Int) {
+        val intent = Intent()
+
+        intent.putExtra("RESULT", getString(stringResId))
+
+        setResult(RESULT_OK, intent)
+
+        finish()
     }
 }
